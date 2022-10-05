89f42b99ec5d12111df6e530864c838d764b0bdf
NauvalSutisna <112870802+NauvalSutisna@users.noreply.github.com>
2022-10-05 19:56:40 +0700

    Add files via upload

---
+++ pertemuan 9.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Tugas Daspro Pertemuan 9" << endl;
+    cout << "Mencari angka kelipatan 5" << endl;
+
+    int k;
+
+    k = 5;
+    while (k <= 100){
+        cout << k << endl;
+        k+=5;
+    }
+    return 0;
+}
